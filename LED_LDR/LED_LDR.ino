461af4e4279254078fd069ddd3eef50566511405
Anix Sam Saji <70740206+anixsam@users.noreply.github.com>
2021-12-14 12:39:42 +0530

    Update LED_LDR.ino

--- LED_LDR/LED_LDR.ino
+++ LED_LDR/LED_LDR.ino
@@ -1,15 +1,13 @@
-float ldr;
+int ldr;
 void setup() 
 {
   pinMode(A0,INPUT);
   pinMode(5,OUTPUT);//D1 pin 
-  Serial.begin(9600);
 }
 
 void loop()
 {
   ldr=analogRead(A0);
-  Serial.println(ldr);
   digitalWrite(5,ldr);
     
 }
